93e1dba19dd7964c868eab0948798fa9f0e7da90
bibs-debug <bia.lima.blm@icloud.com>
2024-09-16 14:26:48 -0800

    Update OperandNode.java

--- OperandNode.java
+++ OperandNode.java
@@ -1,1 +1,12 @@
+package project;
 
+public class OperandNode extends BinaryNode<Float> {
+    public OperandNode(Float value) {
+        super(value);
+    }
+    // Sobrescreve o método visitar para retornar o valor do operando
+    @Override
+    public float visit() {
+        return getValue();
+    }
+}
