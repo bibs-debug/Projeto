8f006fe3236644236b84821176943be57bcd444e
bibs-debug <bia.lima.blm@icloud.com>
2024-09-18 18:44:26 -0300

    Update OperatorNode.java

--- OperatorNode.java
+++ OperatorNode.java
@@ -1,33 +1,24 @@
 //Beatriz Lima de Moura RA: 10416616
 //Beatriz Santos de Souza RA: 10417803
 //Jessica Bispo RA: 10410798
+public class OperatorNode extends BinaryNode {
 
-package project;
-
-public class OperatorNode extends BinaryNode<String> {
-    public OperatorNode(String operator) {
-        super(operator);
+    public OperatorNode(char operator, BinaryNode parent) {
+        super(operator, parent);
     }
-    // Sobrescreve o método visitar para realizar a operação
+    
     @Override
     public float visit() {
-        float leftValue = getLeftNode().visitar();
-        float rightValue = getRightNode().visitar();
-
-        switch (getValue()) {
-            case "+":
-                return leftValue + rightValue;
-            case "-":
-                return leftValue - rightValue;
-            case "*":
-                return leftValue * rightValue;
-            case "/":
-                if (rightValue == 0) {
-                    throw new ArithmeticException("Divisão por zero!");
-                }
-                return leftValue / rightValue;
-            default:
-                throw new IllegalArgumentException("Operador inválido: " + getValue());
+        char value = (char) getValue();
+        if (value == '+') { 
+            return getLeftNode().visit() + getRightNode().visit();
+        } else if (value == '-') {
+            return getLeftNode().visit() - getRightNode().visit();
+        } else if (value == '*') {
+            return getLeftNode().visit() * getRightNode().visit();
+        } else if (value == '/') {
+            return getLeftNode().visit() / getRightNode().visit();
         }
+        return Float.NaN;
     }
 }
